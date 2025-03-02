98fc442cb8ce8f8a5f2dc9ce6933bf8fcb473ce6
boshinomikoto <104365113+boshinomikoto@users.noreply.github.com>
2025-03-02 18:35:34 +0200

    Добавьте файлы проекта.

---
+++ pas lab 2/Source.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+string del_space(string& s) {
+
+	for (int i = 0; i < s.size(); i++) {
+
+		if (isspace(s[i])) {
+			int value = (s.rfind(s[i]) - s.find(s[i]));
+			s.erase(i, value);
+		}
+
+	}
+
+	return s;
+
+}
+
+int main() {
+
+	ifstream fin("text.txt");
+	ofstream fout("text1.txt");
+	ofstream fout1("text2.txt");
+
+	string s;
+
+	while (getline(fin, s)) {
+
+		double n = 0;
+		int m = 0;
+
+		for (int i = 0; i < s.size(); ++i) {
+			if (isdigit(s[i])) {
+				n += s[i] - '0';
+				m++;
+				
+				s.erase(i, 1);
+				
+			}
+
+		}
+
+		double aric_mean = round((n / m) * 10) / 10;
+
+		if (aric_mean >= 4) {
+			fout << del_space(s) << " - " << aric_mean << endl;
+		}
+		if (aric_mean >= 3) {
+			fout1 << del_space(s) << " - " << aric_mean << endl;
+		}
+	}
+
+	fin.close();
+	fout.close();
+	fout1.close();
+
+	return 0;
+
+}
