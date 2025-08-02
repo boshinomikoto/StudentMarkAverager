bee7c2cc42aee76dbbd61c8f0f5c35df464d92ce
boshinomikoto <104365113+boshinomikoto@users.noreply.github.com>
2025-08-02 12:58:19 +0300

    Update Source.cpp

--- pas lab 2/Source.cpp
+++ pas lab 2/Source.cpp
@@ -5,53 +5,47 @@
 
 using namespace std;
 
-string del_space(string& s) {
-
-	for (int i = 0; i < s.size(); i++) {
-
-		if (isspace(s[i])) {
+string del_space(string& s)
+{
+	for (int i = 0; i < s.size(); i++) 
+	{
+		if (isspace(s[i])) 
+		{
 			int value = (s.rfind(s[i]) - s.find(s[i]));
 			s.erase(i, value);
 		}
-
 	}
-
 	return s;
-
 }
 
-int main() {
-
+int main() 
+{
 	ifstream fin("text.txt");
 	ofstream fout("text1.txt");
 	ofstream fout1("text2.txt");
 
 	string s;
 
-	while (getline(fin, s)) {
-
+	while (getline(fin, s)) 
+	{
 		double n = 0;
 		int m = 0;
 
-		for (int i = 0; i < s.size(); ++i) {
-			if (isdigit(s[i])) {
+		for (int i = 0; i < s.size(); ++i)
+		{
+			if (isdigit(s[i]))
+			{
 				n += s[i] - '0';
 				m++;
-				
 				s.erase(i, 1);
-				
 			}
-
 		}
 
 		double aric_mean = round((n / m) * 10) / 10;
-
-		if (aric_mean >= 4) {
+		if (aric_mean >= 4) 
 			fout << del_space(s) << " - " << aric_mean << endl;
-		}
-		if (aric_mean >= 3) {
+		if (aric_mean >= 3) 
 			fout1 << del_space(s) << " - " << aric_mean << endl;
-		}
 	}
 
 	fin.close();
